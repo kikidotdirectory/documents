258c0b17f4775362147945919eac4702e95a8dea
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-26 01:31:16 -0500

    install sass

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,7 +1,7 @@
-export default async function (eleventyConfig) {
-  eleventyConfig
-    .addPassthroughCopy("./css/")
+import path from "node:path";
+import * as sass from "sass";
 
+export default async function (eleventyConfig) {
   // enable smart quotes
   eleventyConfig.amendLibrary("md", function (md) {
     md.set({
@@ -9,6 +9,29 @@ export default async function (eleventyConfig) {
     });
   });
 
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    useLayouts: false,
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+      // Don’t compile file names that start with an underscore
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      const compiled = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      });
+
+      // Map dependencies for incremental builds
+      this.addDependencies(inputPath, compiled.loadedUrls);
+
+      return async (data) => {
+        return compiled.css;
+      };
+    },
+  });
+  eleventyConfig.addTemplateFormats("scss");
 
   eleventyConfig.addCollection("writings", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/writings/**/*.md");
