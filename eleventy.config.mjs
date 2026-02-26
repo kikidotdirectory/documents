0a00f29e20e7f652db1768198c9c06c94b0f29e4
kiki <142045045+kikidotdirectory@users.noreply.github.com>
2026-02-25 22:05:01 -0500

    Initial commit

---
+++ eleventy.config.mjs
@@ -0,0 +1,26 @@
+export default async function (eleventyConfig) {
+  eleventyConfig
+    .addPassthroughCopy({
+      "./public/": "/"
+    })
+    .addPassthroughCopy("**/*.png")
+
+  // enable smart quotes
+  eleventyConfig.amendLibrary("md", function (md) {
+    md.set({
+      typographer: true,
+    });
+  });
+}
+
+export const config = {
+  markdownTemplateEngine: "njk",
+  htmlTemplateEngine: "njk",
+  dir: {
+    input: "content",
+    includes: "../_includes",
+    layouts: "../_includes/layouts",
+    data: "../_data",
+    output: "_site"
+  }
+}
