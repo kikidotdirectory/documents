f075bff98c9b3f74940c30693173601574049095
kiki <142045045+calciferchang@users.noreply.github.com>
2026-02-26 18:23:55 -0500

    add andy bell's tailwind cube configuration

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,6 +1,28 @@
 import { RenderPlugin } from "@11ty/eleventy";
 
+import fs from "node:fs/promises";
+import path from "node:path";
+
+import postcss from "postcss";
+import postcssImportExtGlob from "postcss-import-ext-glob";
+import postcssImport from "postcss-import";
+import tailwindcss from "tailwindcss";
+
 export default async function (eleventyConfig) {
+	eleventyConfig.on("eleventy.before", async () => {
+		const inputPath = "src/css/global.css";
+		const outputPath = "dist/css/global.css";
+		const inputContent = await fs.readFile(inputPath, "utf-8");
+		const result = await postcss([
+			postcssImportExtGlob,
+			postcssImport,
+			tailwindcss,
+		]).process(inputContent, { from: inputPath });
+
+		await fs.mkdir(path.dirname(outputPath), { recursive: true });
+		await fs.writeFile(outputPath, result.css);
+	});
+
 	// enable smart quotes
 	eleventyConfig.amendLibrary("md", function (md) {
 		md.set({
@@ -10,9 +32,6 @@ export default async function (eleventyConfig) {
 
 	// Enable plugins
 	eleventyConfig.addPlugin(RenderPlugin);
-
-	// Enable CSS bundling
-	eleventyConfig.addBundle("css");
 
 	eleventyConfig.addCollection("writings", function (collectionApi) {
 		return collectionApi.getFilteredByGlob("src/writings/**/*.md");
